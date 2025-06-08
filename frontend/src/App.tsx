fe2eb95a8612c647cec31478f821e7a668283e89
alexdreyesz <alexdreyesz@gmail.com>
2025-06-07 23:09:37 -0400

    Added Router For Landing and User Profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Landing from "./pages/Landing/Landing"
+import UserProfile from "./pages/UserProfile/UserProfile"
 
 function App() {
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
